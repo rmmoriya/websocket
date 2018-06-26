7801275fff207a52ffe6785f3a1d1e492e3c6a50
Rafael Moriya <rm.moriya@gmail.com>
2018-06-26 16:18:28 -0300

    (Feature) Websocket WSS

--- client.php
+++ client.php
@@ -3,6 +3,6 @@ require __DIR__ . '/vendor/autoload.php';
 
 use WSSC\WebSocketClient;
 
-$client = new WebSocketClient('wss://websocket.augusto.ai/notifications/messanger/yourtoken123');
-$client->send('{"room" : 5}');
+$client = new WebSocketClient('wss://websocket.augusto.ai');
+$client->send('{"room" : 3}');
 echo $client->receive();
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
-$websocketServer = new \WSSC\WebSocketServer(new ServerHandler(), [
+$websocketServer = new \WSSC\WebSocketServer(new \ServerHandler(), [
     'host' => '0.0.0.0',
     'port' => 8000
 ]);
