2e6e1db8f1ab19e98ad84686ee68b0dc69202abe
Rafael Moriya <rm.moriya@gmail.com>
2018-06-26 14:29:35 -0300

    (Feature) Websocket

--- client.php
+++ client.php
@@ -3,6 +3,6 @@ require __DIR__ . '/vendor/autoload.php';
 
 use WSSC\WebSocketClient;
 
-$client = new WebSocketClient('ws://websocket.augusto.ai/notifications/messanger/yourtoken123');
+$client = new WebSocketClient('ws://localhost:8000/notifications/messanger/yourtoken123');
 $client->send('{"user_id" : 123}');
 echo $client->receive();
