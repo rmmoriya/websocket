d0c05a574651202dadf30873be261198caedcfbd
Rafael Moriya <rm.moriya@gmail.com>
2018-06-26 19:03:02 -0300

    (Feature) Send message to room

--- ServerHandler.php
+++ ServerHandler.php
@@ -11,6 +11,7 @@ class ServerHandler extends WebSocket
 
     public function onOpen(ConnectionContract $conn)
     {
+        echo"<pre>"; var_dump($this->pathParams[':token']);
         $this->clients[$this->pathParams[':token']][] = $conn;
         $conn->send(json_encode([
             "id" => "eb4e0ec3",
