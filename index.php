fda9f770f18360a30e44c94a151b6a1170c5d7ff
Rafael Moriya <rm.moriya@gmail.com>
2018-06-26 16:19:28 -0300

    (Feature) Websocket WSS

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
+require_once __DIR__ . 'ServerHandler.php';
+
 $websocketServer = new \WSSC\WebSocketServer(new \ServerHandler(), [
     'host' => '0.0.0.0',
     'port' => 8000
