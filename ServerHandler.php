ebc275e06a6af52d05e40435998d39d7230f265c
Rafael Moriya <rm.moriya@gmail.com>
2018-06-26 16:45:36 -0300

    (Feature) Index html

--- ServerHandler.php
+++ ServerHandler.php
@@ -20,6 +20,7 @@ class ServerHandler extends WebSocket
     public function onOpen(ConnectionContract $conn)
     {
         $this->clients[] = $conn;
+        echo"<pre>"; var_dump($this->pathParams);
         echo json_encode([
             "id" => "eb4e0ec3",
             "event" => "open",
